d33d3748f3fa8497ab1cea6eb8820b063037eaff
Mackinlay Kevin <kemack83@gmail.com>
2023-09-07 17:05:39 -0300

    adding componentes y react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
-import "./App.css";
-
-/* function App() {
-  let logueado = true; */
-//Logica sin sugar sintax
-/* const estadoLogueado = () => {
-    if (logueado == false) {
-      return (
-        <button className="">
-          <p>Aceptar</p>
-        </button>
-      );
-    } else {
-      return (
-        <div>
-          <button className="">
-            <p>Cancelar</p>
-          </button>
-          
-        </div>
-      );
-    }
-  }; */
-/*   return (
-    <div className="App">
-      <h1>Mi proyecto React de CoderHouse</h1>
-      {logueado == true ? (
-        <button className="">
-          <p>Aceptar</p>
-        </button>
-      ) : (
-        <button className="">
-          <p>Cancelar</p>
-        </button>
-      )}
-    </div>
-  );
-}
- */
-import FormularioLogin from "./componentes/formularios/formularioLogin"
-import FormularioRegistrarse from "./componentes/formularios/formularioRegistrarse"
-//componente
-
-//componente
-
+import Navbar from "./componentes/Navbar/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+/* import FormularioLogin from "./componentes/formularios/formularioLogin"
+import FormularioRegistrarse from "./componentes/formularios/formularioRegistrarse" */
 
 function App() {
-  let tipoFormulario = "login";
+ /*  let tipoFormulario = "login"; */
   return (
     <div>
-      <h1> Mi Proyecto</h1>
-      {tipoFormulario === "login" ? <FormularioLogin /> : <FormularioRegistrarse />}
+      <Navbar texto="soy un texto" />
     </div>
+    /*   <div>
+      <h1 className="titulo" style={{padding:10}}> Mi Proyecto</h1>
+      {tipoFormulario === "login" ? <FormularioLogin /> : <FormularioRegistrarse />}
+    </div> */
   );
 }
 export default App;
